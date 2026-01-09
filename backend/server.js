e31940494de07323a951057351db0be44be70801
bhqn <bernardo_quintanilha@hotmail.com>
2026-01-09 14:54:25 -0300

    versao 8.10.1

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const app = require("./app");
 
 // Conexão MongoDB
 const MONGO_URI = process.env.MONGO_URI;
-mongoose
-  .connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("MongoDB conectado com sucesso"))
   .catch((err) => console.error("Erro ao conectar no MongoDB:", err));
 
